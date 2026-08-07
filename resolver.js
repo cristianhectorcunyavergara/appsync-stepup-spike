7575a561e657fd53bbe13e919158be0c48ddda9d
Cristian Hector Cunya Vergara <cristiancunya@bcp.com.pe>
2026-08-07 17:13:03 -0500

    Create resolver.js
    Create resolver.js appsync

---
+++ resolver.js
@@ -0,0 +1,18 @@
+import { executeMiddleware } from "./middleware.js";
+
+export function request(ctx) {
+
+    const result = executeMiddleware(ctx);
+
+    if (result.stopExecution) {
+        return result;
+    }
+
+    return {
+        payload: {}
+    };
+}
+
+export function response(ctx) {
+    return ctx.result;
+}
